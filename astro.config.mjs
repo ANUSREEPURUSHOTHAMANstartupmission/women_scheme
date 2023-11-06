377550873421783f8ff7b34ba80ba7a06a0fe6b4
ANUSREEPURUSHOTHAMANstartupmission <121005558+ANUSREEPURUSHOTHAMANstartupmission@users.noreply.github.com>
2023-11-06 13:11:30 +0530

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
@@ -5,5 +6,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ANUSREEPURUSHOTHAMANstartupmission.github.io',
+  base: '/women_scheme',
   integrations: [tailwind(), svelte()]
 });
